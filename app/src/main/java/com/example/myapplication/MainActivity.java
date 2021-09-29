0cbac9bbc565481b205b9511129d401702b401d7
jorge <quechedo@gmail.com>
2021-09-29 16:33:13 +0200

    Version contraintLayout

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int contador;
+    private TextView numero = null;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contador = 0;
+        numero = (TextView) findViewById(R.id.txt_numero);
+    }
+
+    public void contar(View view)
+    {
+        contador++;
+        numero.setText(String.valueOf(contador));
+    }
+
+    public void resetear(View view)
+    {
+        contador = 0;
+        numero.setText('0');
+    }
+}
